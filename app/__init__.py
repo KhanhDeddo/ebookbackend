b6045613fcbcc0125a075e7f89f461f5eddd88e7
Khanh Deddo <khanhdeddo@gmail.com>
2024-11-22 23:11:19 +0700

    aaaa

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,6 @@ def create_app():
     db.init_app(app)
 
     from .routes import api_bp
-    app.register_blueprint(api_bp, url_prefix='/')
+    app.register_blueprint(api_bp, url_prefix='/api')
 
     return app
